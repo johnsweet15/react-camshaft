28378ae86d9b99e35616c7e827d6d7e821e06190
johnsweet15 <johntsweet15@gmail.com>
2022-07-29 19:40:18 -0400

    Added framer motion

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React from 'react';
+import { motion } from 'framer-motion';
 import './Button.scss';
 
 export interface ButtonProps {
@@ -6,7 +7,22 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <motion.button
+      whileHover={{
+        cursor: 'pointer',
+        scale: 1.05,
+        transition: { duration: 0.2 },
+      }}
+      whileTap={{
+        opacity: 0.8,
+        transition: { duration: 0.1 },
+        scale: 1,
+      }}
+    >
+      {props.label}
+    </motion.button>
+  );
 };
 
 export default Button;
