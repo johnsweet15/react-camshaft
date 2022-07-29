6a9bbbfb50865f358baaddd2cb5e32c27518249c
johnsweet15 <johntsweet15@gmail.com>
2022-07-29 19:11:55 -0400

    Finished setup: added storybook and scss

--- rollup.config.js
+++ rollup.config.js
@@ -42,6 +42,6 @@ export default [
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+    external: [/\.s?css$/],
   },
 ];
